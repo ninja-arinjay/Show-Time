7c99e8793f501b91055e42cb0e9fc852700c8f6c
Arinjay Panwar <arinjaypanwararp@gmail.com>
2024-01-06 19:14:41 -0500

    Added about page section

---
+++ src/app/about/page.jsx
@@ -0,0 +1,30 @@
+export default function About() {
+  return (
+    <div className="max-w-6xl mx-auto space-y-4 p-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to Show Time website! We are a team of passionate movie
+        enthusiasts who have come together to create a one-stop destination for
+        all your movie-related needs.
+      </p>
+
+      <p>
+        Our platform offers an extensive movie database featuring films from various corners of the globe, accompanied by the most recent news, reviews, and trailers. The movie database undergoes regular updates with the addition of new releases, ensuring that you stay informed about the latest developments in the world of cinema. Easily discover movies by searching through titles, directors, actors, genres, or release dates, providing a convenient way to find the ideal film for any occasion.
+      </p>
+
+      <p>
+        In addition to our extensive movie database, we also offer a platform
+        for movie lovers to connect and share their thoughts on the latest
+        releases. Our community section includes a forum where you can discuss
+        your favorite films with like-minded individuals and read reviews and
+        ratings from other users. We also have a section dedicated to movie news
+        and trailers, keeping you updated with the latest happenings in the
+        world of cinema. Thank you for visiting our website and we hope you
+        enjoy your time browsing through our movie database. If you have any
+        feedback or suggestions, please feel free to contact us. We are always
+        looking for ways to improve and enhance the user experience on our
+        website. Happy browsing!
+      </p>
+    </div>
+  );
+}
